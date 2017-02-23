96a7dd75b7df8ed59556384ddd597b171e8c9b24
Игорь Талов <igortalov@gmail.com>
2017-02-24 00:46:24 +0300

    Add simple animation

--- LottieAnimationTest/ViewController.swift
+++ LottieAnimationTest/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
-
+    
+    var menuOn = false
+    var hamburgerMenuButton: LOTAnimationView?
+    var hamburgerMenuFrame = CGRect(x: 0, y: 10, width: 75, height: 75)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        addHumburgerMenu(on: menuOn)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,58 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func addHumburgerMenu(on: Bool) {
+        if hamburgerMenuButton != nil {
+            hamburgerMenuButton?.removeFromSuperview()
+            hamburgerMenuButton = nil
+        }
+        
+        let animation = on ? "buttonOn" : "buttonOff"
+        
+        hamburgerMenuButton = LOTAnimationView.animationNamed(animation)
+        hamburgerMenuButton?.isUserInteractionEnabled = true
+        hamburgerMenuButton?.frame = hamburgerMenuFrame
+        hamburgerMenuButton?.contentMode = .scaleAspectFill
+
+        addMenuToggleRecognizer()
+        
+        self.view.addSubview(hamburgerMenuButton!)
+    }
+    
+    func addMenuToggleRecognizer() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.toogleMenu(recognizer:)))
+        
+        tapGesture.numberOfTapsRequired = 1
+        hamburgerMenuButton?.addGestureRecognizer(tapGesture)
+    }
+    
+    func toogleMenu(recognizer: UITapGestureRecognizer) {
+        
+        if !menuOn {
+            hamburgerMenuButton?.play(completion: { (success) in
+                self.menuOn = true
+                self.addHumburgerMenu(on: self.menuOn)
+            })
+        } else {
+            hamburgerMenuButton?.play(completion: { (success) in
+                self.menuOn = false
+                self.addHumburgerMenu(on: self.menuOn)
+            })
+        }
+    }
+    
+    // MARK - Actions
+    @IBAction func showAnimation(_ sender: UIButton) {
+        let animationView = LOTAnimationView.animationNamed("PinJump")
+        animationView?.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 250)
+        animationView?.contentMode = .scaleAspectFill
+        animationView?.loopAnimation = true
+        self.view.addSubview(animationView!)
+        
+        animationView?.play()
+    }
+    
+    
 
 }
 
